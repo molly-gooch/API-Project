9d0ef217f4a548e2f25bc5148092507690b89ecc
Molly Gooch <molly_gooch26@milton.edu>
2024-12-05 14:02:13 -0500

    HW done

--- src/Project.java
+++ src/Project.java
@@ -25,16 +25,22 @@ public class Project {
     private int WIDTH = 800;
     private int HEIGHT = 700;
     private JPanel panel1;
+    private JPanel panel101;
+    private JPanel panel102;
+    private JPanel panel103;
     private JPanel panel2;
     private JPanel panel3;
     private JPanel panel4;
 
     private JButton b1;
     private JButton b2;
+    private JButton b3;
+    private JButton b4;
 
     private JTextArea ta1;
     private JTextArea ta2;
     private JTextArea ta3;
+
 
 
     public Project() {
@@ -51,6 +57,9 @@ public class Project {
         mainFrame.setSize(WIDTH, HEIGHT);
         mainFrame.setLayout(new GridLayout(3, 1));
         panel1 = new JPanel();
+        panel101 = new JPanel(new BorderLayout());
+        panel102 = new JPanel(new BorderLayout());
+        panel103 = new JPanel(new BorderLayout());
         panel2 = new JPanel();
         panel3 = new JPanel();
         panel4 = new JPanel();
@@ -65,47 +74,99 @@ public class Project {
         ta3 = new JTextArea();
 
         mainFrame.add(panel1);
-        panel1.add(ta1);
-        panel1.add(ta2);
-        panel1.add(ta3);
+        panel101.add(ta1);
+        panel1.add(panel101, BorderLayout.CENTER);
+        panel1.add(panel102, BorderLayout.CENTER);
+        panel102.add(ta2);
+        panel103.add(ta3);
+        panel1.add(panel103, BorderLayout.CENTER);
         mainFrame.add(panel2);
         mainFrame.add(panel3);
 
     }
 
     public void pull() throws ParseException {
-        String output = "abc";
-        String totlaJson = "";
+        String output1 = "abc";
+        String output2 = "abc";
+        String output3 = "abc";
+        String totlaJson1 = "";
+        String totlaJson2 = "";
+        String totlaJson3 = "";
 
 
         String apiKey = "6fG+e/V/5n45lzPE5UKMCQ==ecwp26yUZKx9j9Ur";
 
         try {
 
-            URL url = new URL("https://api.api-ninjas.com/v1/animals?name=" + ta1.getText());
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-            conn.setRequestProperty("X-Api-Key", apiKey);
+            URL url1 = new URL("https://api.api-ninjas.com/v1/animals?name=" + ta1.getText());
+            URL url2 = new URL("https://api.api-ninjas.com/v1/animals?name=" + ta2.getText());
+            URL url3 = new URL("https://api.api-ninjas.com/v1/animals?name=" + ta3.getText());
+            HttpURLConnection conn1 = (HttpURLConnection) url1.openConnection();
+            HttpURLConnection conn2 = (HttpURLConnection) url2.openConnection();
+            HttpURLConnection conn3 = (HttpURLConnection) url3.openConnection();
+
+            conn1.setRequestMethod("GET");
+            conn1.setRequestProperty("Accept", "application/json");
+            conn1.setRequestProperty("X-Api-Key", apiKey);
+
+            conn2.setRequestMethod("GET");
+            conn2.setRequestProperty("Accept", "application/json");
+            conn2.setRequestProperty("X-Api-Key", apiKey);
+
+            conn3.setRequestMethod("GET");
+            conn3.setRequestProperty("Accept", "application/json");
+            conn3.setRequestProperty("X-Api-Key", apiKey);
 
 
-            if (conn.getResponseCode() != 200) {
+            if (conn1.getResponseCode() != 200) {
 
                 throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
+                        + conn1.getResponseCode());
+            }
+
+            if (conn2.getResponseCode() != 200) {
+
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn2.getResponseCode());
+            }
+
+            if (conn3.getResponseCode() != 200) {
+
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn3.getResponseCode());
             }
 
             BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
+                    (conn1.getInputStream())));
+
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(
+                    (conn2.getInputStream())));
+
+            BufferedReader br3 = new BufferedReader(new InputStreamReader(
+                    (conn3.getInputStream())));
 
 
             System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-                totlaJson += output;
+            while ((output1 = br.readLine()) != null) {
+                System.out.println(output1);
+                totlaJson1 += output1;
             }
 
-            conn.disconnect();
+            System.out.println("Output from Server .... \n");
+            while ((output2 = br2.readLine()) != null) {
+                System.out.println(output2);
+                totlaJson2 += output2;
+            }
+
+            System.out.println("Output from Server .... \n");
+            while ((output3 = br3.readLine()) != null) {
+                System.out.println(output3);
+                totlaJson3 += output3;
+            }
+
+            conn1.disconnect();
+            conn2.disconnect();
+            conn3.disconnect();
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -114,70 +175,31 @@ public class Project {
             e.printStackTrace();
         }
 
-        JSONParser parser = new JSONParser();
-        org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) parser.parse(totlaJson);
-        System.out.println(jsonObject);
+        JSONParser parser1 = new JSONParser();
+        org.json.simple.JSONObject jsonObject1 = (org.json.simple.JSONObject) parser1.parse(totlaJson1);
+        System.out.println(jsonObject1);
+
+        JSONParser parser2 = new JSONParser();
+        org.json.simple.JSONObject jsonObject2 = (org.json.simple.JSONObject) parser2.parse(totlaJson2);
+        System.out.println(jsonObject2);
+
+        JSONParser parser3 = new JSONParser();
+        org.json.simple.JSONObject jsonObject3 = (org.json.simple.JSONObject) parser3.parse(totlaJson3);
+        System.out.println(jsonObject3);
 
         try {
 
-            String name = (String) jsonObject.get("name");
-            Long weight = (Long) jsonObject.get("weight");
-            Long height = (Long) jsonObject.get("height");
+            String name1 = (String) jsonObject1.get("name");
+            System.out.println(name1);
 
 
-            String eye = (String) jsonObject.get("eye_color");
-            String birth = (String) jsonObject.get("birth_year");
-            System.out.println();
 
-            ta2.append("Pokemon name: " + name);
-            ta2.append("\n");
-            ta2.append("Pokemon weight: " + weight);
-            ta2.append("\n");
-            ta2.append("Pokemon height: " + height);
-            ta2.append("\n");
+            String name2 = (String) jsonObject2.get("name");
+            System.out.println(name2);
 
+            String name3 = (String) jsonObject3.get("name");
+            System.out.println(name3);
 
-            org.json.simple.JSONArray ab = (org.json.simple.JSONArray) jsonObject.get("abilities");
-            int n = ab.size(); //(msg).length();a
-            for (int i = 0; i < n; ++i) {
-                JSONObject test = (JSONObject) ab.get(i);
-                JSONObject ability = (JSONObject) test.get("ability");
-                ta2.append("Ability " + (i + 1) + ": " + ability.get("name"));
-                ta2.append("\n");
-            }
-
-
-            org.json.simple.JSONArray forms = (org.json.simple.JSONArray) jsonObject.get("forms");
-            int f = forms.size();
-            for (int i = 0; i < f; i = i + 1) {
-                JSONObject test2 = (JSONObject) forms.get(i);
-                String names = (String) test2.get("name");
-                //String links = (String) test2.get("url");
-                ta2.append("Form " + (i + 1) + ": " + names);
-                ta2.append("\n");
-                // ta2.append("Form link: " + links);
-                // ta2.append("\n");
-            }
-
-            org.json.simple.JSONArray type = (org.json.simple.JSONArray) jsonObject.get("types");
-            int t = type.size();
-            for (int i = 0; i < t; i = i + 1) {
-                JSONObject test3 = (JSONObject) type.get(i);
-                JSONObject types = (JSONObject) test3.get("type");
-                String names = (String) types.get("name");
-                ta2.append("Type " + (i + 1) + ": " + names);
-                ta2.append("\n");
-            }
-
-            org.json.simple.JSONArray move = (org.json.simple.JSONArray) jsonObject.get("moves");
-            int m = type.size();
-            for (int i = 0; i < m; i = i + 1) {
-                JSONObject test4 = (JSONObject) move.get(i);
-                JSONObject moves = (JSONObject) test4.get("move");
-                String movesy = (String) moves.get("name");
-                ta2.append("Move " + (i + 1) + ": " + movesy);
-                ta2.append("\n");
-            }
 
 
         } catch (Exception e) {
@@ -189,23 +211,28 @@ public class Project {
 
         b1 = new JButton("Submit");
         b2 = new JButton("Reset");
+        b3 = new JButton("Reset");
+        b4 = new JButton("Reset");
         b1.setActionCommand("Submit");
-        b2.setActionCommand("Reset");
+        b2.setActionCommand("Reset1");
+        b3.setActionCommand("Reset2");
+        b4.setActionCommand("Reset3");
         b1.addActionListener(new ButtonClickListener());
         b2.addActionListener(new ButtonClickListener());
+        b3.addActionListener(new ButtonClickListener());
+        b4.addActionListener(new ButtonClickListener());
         b1.setFont(new Font("Calibri", Font.BOLD, 30));
         b2.setFont(new Font("Calibri", Font.BOLD, 30));
+        b3.setFont(new Font("Calibri", Font.BOLD, 30));
+        b4.setFont(new Font("Calibri", Font.BOLD, 30));
 
 
-        b1.setActionCommand("Submit");
-        b2.setActionCommand("Reset");
-
-
-        b1.addActionListener(new ButtonClickListener());
-        b2.addActionListener(new ButtonClickListener());
 
         panel2.add(b1);
-        panel2.add(b2);
+        panel101.add(b2, BorderLayout.SOUTH);
+        panel102.add(b3, BorderLayout.SOUTH);
+        panel103.add(b4, BorderLayout.SOUTH);
+
 
         mainFrame.setVisible(true);
     }
@@ -221,6 +248,18 @@ public class Project {
                 } catch (Exception e) {
                     System.out.println(e);
                 }
+            }
+
+            if(command.equals("Reset1")){
+                ta1.setText("");
+            }
+
+            if(command.equals("Reset2")){
+                ta2.setText("");
+            }
+
+            if(command.equals("Reset3")){
+                ta3.setText("");
             }
 
         }
